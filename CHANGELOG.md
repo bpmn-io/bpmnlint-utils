cb6223912c4b135a9afb6ae22ec5a8348e2d3a54
Nico Rehwaldt <git_nikku@nixis.de>
2018-09-24 21:55:01 +0200

    chore(project): add initial implementation

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+All notable changes to [bpmnlint-utils](https://github.com/bpmn-io/bpmnlint-utils) are documented here. We use [semantic versioning](http://semver.org/) for releases.
+
+## Unreleased
+
+___Note:__ Yet to be released changes appear here._
+
+## 1.0.0
+
+* `FEAT`: initial version :tada:
---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import pkg from './package.json';
+
+function pgl(plugins=[]) {
+  return [
+    ...plugins
+  ];
+}
+
+export default [
+  {
+    input: 'lib/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs', sourcemap: true },
+      { file: pkg.module, format: 'es', sourcemap: true }
+    ],
+    plugins: pgl()
+  }
+];
